ef232b1671d1e6615b5578bb78be4f82996aefc7
MestreLion <linux@rodrigosilva.com>
2024-06-19 03:17:32 -0300

    bug/run: output tests to out/, use sudo on pv

--- bug/run.sh
+++ bug/run.sh
@@ -23,8 +23,8 @@ mkdir -p -- "$outdir"
 escape()  { printf '%q' "$1"; }  # $@ will not work as expected
 
 run() {
-	pseudo="zeroes.img f 444 0 0 $(escape "$pv") -Ss 10G /dev/zero"
-	tmpfile=${here}/test_${mksquashfs##*/}_${pv##*/}.sqsh
+	pseudo="zeroes.img f 444 0 0 sudo $(escape "$pv") -Ss 10G /dev/zero"
+	tmpfile=${outdir}/test_${mksquashfs##*/}_${pv##*/}.sqsh
 	rm -f "$tmpfile"
 	"$mksquashfs" "$tmpdir" "$tmpfile" -info -p "$pseudo"
 }
